cdd3d65e3806526acac77884e177766533d95b66
KunalJhatta <Kunaljhatta22@gmail.com>
2022-05-08 20:34:14 -0700

    Apply Introduce Parameter object for button for long parameter list

--- main.rb
+++ main.rb
@@ -1,7 +1,16 @@
 def draw_dark_mode(label_text, x, y, foreground_color)
-    paint(label_text, x, y, foreground_color - 10, '#111111')
+    paint(button.label_text, button.x, button.y, button.foreground_color - 10, '#111111')
 end
 
 def draw_light_mode(label_text, x, y, foreground_color)
-  paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+  paint(button.label_text, button.x, button.y, button.foreground_color + 10, '#E0E0E0')
 end
+
+class Button
+    attr_reader :label_text, :x, :y, :foreground_color
+    def intialize(label_text, x, y, foreground_color)
+      @label_text = label_text
+      @x = x
+      @y = y
+      @foreground_color = foreground_color
+    end
