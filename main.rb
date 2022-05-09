66b42bd6bc6b7415bfff8cf44000ba858fcb50bd
KunalJhatta <Kunaljhatta22@gmail.com>
2022-05-08 20:30:31 -0700

    Apply extract function and remove flag to create two seperate functions

--- main.rb
+++ main.rb
@@ -1,9 +1,7 @@
-def draw_button(label_text, x, y, foreground_color, is_dark_mode)
-    if is_dark_mode
-      # darken foreground color for dark mode
-      paint(label_text, x, y, foreground_color - 10, '#111111')
-    else
-      # lighten foreground color for non-dark mode
-      paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
-    end
-  end
+def draw_dark_mode(label_text, x, y, foreground_color)
+    paint(label_text, x, y, foreground_color - 10, '#111111')
+end
+
+def draw_light_mode(label_text, x, y, foreground_color)
+  paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+end
